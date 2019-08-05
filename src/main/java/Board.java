dfe13a83829eded353a3fb57e21ef644da7ab9b2
mrlukasbos <mrlukasbos@gmail.com>
2019-08-06 00:44:33 +0200

    more files in maven structure

---
+++ src/main/java/Board.java
@@ -0,0 +1,97 @@
+// a board consists of multiple tiles
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+class Board {
+    private Tile[][] tiles = new Tile[7][];
+
+    // for each player we need
+
+
+    Board() {
+        // lets construct a board (rows x cols)
+        tiles[0] = new Tile[4];
+        tiles[0][0] = new Tile(Type.SEA);
+        tiles[0][1] = new Tile(Type.SEA);
+        tiles[0][2] = new Tile(Type.SEA);
+        tiles[0][3] = new Tile(Type.SEA);
+
+        tiles[1] = new Tile[5];
+        tiles[1][0] = new Tile(Type.SEA);
+        tiles[1][1] = new Tile(Type.GRAIN, 8);
+        tiles[1][2] = new Tile(Type.STONE, 4);
+        tiles[1][3] = new Tile(Type.GRAIN, 5);
+        tiles[1][4] = new Tile(Type.SEA);
+
+        tiles[2] = new Tile[6];
+        tiles[2][0] = new Tile(Type.SEA);
+        tiles[2][1] = new Tile(Type.STONE, 11);
+        tiles[2][2] = new Tile(Type.WHOOL, 2);
+        tiles[2][3] = new Tile(Type.GRAIN, 11);
+        tiles[2][4] = new Tile(Type.WHOOL, 9);
+        tiles[2][5] = new Tile(Type.SEA);
+
+        tiles[3] = new Tile[7];
+        tiles[3][0] = new Tile(Type.SEA);
+        tiles[3][1] = new Tile(Type.WOOD, 10);
+        tiles[3][2] = new Tile(Type.ORE, 6);
+        tiles[3][3] = new Tile(Type.DESERT);
+        tiles[3][4] = new Tile(Type.ORE, 3);
+        tiles[3][5] = new Tile(Type.WOOD, 8);
+        tiles[3][6] = new Tile(Type.SEA);
+
+        tiles[4] = new Tile[6];
+        tiles[4][0] = new Tile(Type.SEA);
+        tiles[4][1] = new Tile(Type.WHOOL, 5);
+        tiles[4][2] = new Tile(Type.WOOD, 3);
+        tiles[4][3] = new Tile(Type.STONE, 9);
+        tiles[4][4] = new Tile(Type.GRAIN, 4);
+        tiles[4][5] = new Tile(Type.SEA);
+
+        tiles[5] = new Tile[5];
+        tiles[5][0] = new Tile(Type.SEA);
+        tiles[5][1] = new Tile(Type.WOOD, 6);
+        tiles[5][2] = new Tile(Type.ORE, 10);
+        tiles[5][3] = new Tile(Type.WHOOL, 12);
+        tiles[5][3] = new Tile(Type.SEA);
+
+        tiles[6] = new Tile[4];
+        tiles[6][0] = new Tile(Type.SEA);
+        tiles[6][1] = new Tile(Type.SEA);
+        tiles[6][2] = new Tile(Type.SEA);
+        tiles[6][3] = new Tile(Type.SEA);
+    }
+
+    void placeVillage(Player p) {
+
+    }
+
+    void placeCity(Player p) {
+
+    }
+
+    void placeStreet(Player p) {
+
+    }
+
+    @Override
+    public String toString() {
+        String tilesString = "[";
+        // TODO: remove hardcoded values
+        for (int i = 0; i < 7; i++) {
+            tilesString = tilesString.concat(Arrays.toString(tiles[i]));
+            if (i < 6) {
+                tilesString = tilesString.concat(",");
+            }
+        }
+        tilesString = tilesString.concat("]");
+
+        return "{" +
+                "\"model\": \"board\"," +
+                "\"attributes\": {" +
+                "\"tiles\": " + tilesString +
+                "}" +
+                '}';
+    }
+}
---
+++ src/main/java/Tile.java
@@ -0,0 +1,111 @@
+// a tile can either be land
+
+class Tile {
+    private Type type;
+    private int number; // the number of the dice to be hit
+    private Tile[] sides = new Tile[6];
+
+    Tile(Type type) {
+        this.type = type;
+        this.number = 0;
+    }
+
+    Tile(Type type, int number) {
+        this.type = type;
+        this.number = number;
+    }
+
+    void setSides(Tile[] sides) {
+        assert sides.length == 6;
+        this.sides = sides;
+    }
+
+    public String typeToString(Type type) {
+        switch (type) {
+            case DESERT:
+                return "desert";
+            case WHOOL:
+                return "whool";
+            case WOOD:
+                return "wood";
+            case STONE:
+                return "stone";
+            case GRAIN:
+                return "grain";
+            case ORE:
+                return "ore";
+            case SEA:
+                return "sea";
+            case HARBOUR_WHOOL:
+                return "harbour_whool";
+            case HARBOUR_WOOD:
+                return "harbour_wood";
+            case HARBOUR_STONE:
+                return "harbour_stone";
+            case HARBOUR_GRAIN:
+                return "harbour_grain";
+            case HARBOUR_ORE:
+                return "harbour_ore";
+            case HARBOUR_ALL:
+                return "harbour_all";
+            default:
+                return "unknown";
+        }
+    }
+
+    public String orientationToString(Orientation orientation) {
+        switch (orientation) {
+            case TOP_LEFT:
+                return "top_left";
+            case TOP_RIGHT:
+                return "top_right";
+            case LEFT:
+                return "left";
+            case RIGHT:
+                return "right";
+            case BOTTOM_LEFT:
+                return "bottom_left";
+            case BOTTOM_RIGHT:
+                return "bottom_right";
+            default:
+                return "unknown";
+        }
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "{" +
+                "\"model\": \"tile\"," +
+                "\"attributes\": {" +
+                "\"type\": \"" + typeToString(this.type) + "\"," +
+                "\"number\": " + number +
+                "}" +
+                "}";
+    }
+}
+
+enum Type {
+    DESERT,
+    WHOOL,
+    WOOD,
+    STONE,
+    GRAIN,
+    ORE,
+    SEA,
+    HARBOUR_WHOOL,
+    HARBOUR_WOOD,
+    HARBOUR_STONE,
+    HARBOUR_GRAIN,
+    HARBOUR_ORE,
+    HARBOUR_ALL
+}
+
+// used for harbours
+enum Orientation {
+    TOP_LEFT,
+    TOP_RIGHT,
+    LEFT,
+    RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_RIGHT
+}
