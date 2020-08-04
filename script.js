2cdd8ac0a024797e7b25efe3e336028a69c42c8f
mrlukasbos <mrlukasbos@gmail.com>
2020-08-04 22:36:49 +0200

    add translations and style updates 2

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-var lang = 'EN';
-
-// lowercase translation
-function t(key) {
-    if (lang == 'NL') { return t_NL[key];}
-    else{ return t_EN[key];}
-}
-
-// uppercase translation
-function T(key) {
-    var str = t(key);
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
 var json = null;
 
 function draw() {
@@ -98,7 +84,7 @@ svg.append("g")
         var str = "";
         if (d.tile.attributes.resource_type === "SEA") {
             if (d.tile.attributes.harbour_type !== "HARBOUR_NONE") {
-                str = t(d.tile.attributes.harbour_type);
+                str = d.tile.attributes.harbour_type.toLowerCase();
             }
         } else {
             str = d.tile.attributes.resource_type.toLowerCase();
@@ -276,14 +262,6 @@ function hexProjection(radius) {
 }
 
 
-
-Vue.mixin({
-    methods: {
-      t: str => t(str),
-      T: str => T(str)
-    }
-  })
-
 var app = new Vue({
     el: '#app',
     data: {
@@ -298,27 +276,7 @@ var app = new Vue({
         lastDiceThrow: 0,
         currentPlayerId: null,
         events: [],
-        moveCount: 0,
-        colors: [
-            {
-                name: "yellow", 
-                code: "#ffd218"
-            }, 
-            {
-                name: "red", 
-                code: "#ff0000"
-            }, 
-            {
-                name: "green", 
-                code: "#00ff00"
-            }, 
-            {
-                name: "blue", 
-                code: "#0000ff"
-            },
-        ],
-        selectedColorCode: "#ffd218",
-        joinModalVisible: false,
+        moveCount: 0
     },
     methods: {
         connect: function (event) {
@@ -392,21 +350,8 @@ var app = new Vue({
                 evt.attributes.player_full = complete_player;
                 return evt;
             });
-        },
-        selectColor: function(evt, color) {
-            this.selectedColorCode = color.code;
-        }, 
-
-        showJoinModal: function() {
-            this.joinModalVisible = true;
-        },
-        hideJoinModal: function() {
-            this.joinModalVisible = false;
-        },
-
-        joinGame: function() {
-
         }
+
     },
     mounted: function() {
         if (localStorage.ip) {
@@ -419,5 +364,3 @@ var app = new Vue({
 
     }
 });
-
-
