ef6b371d5a48e3f2701f9e602f88b7ede9514e00
mrlukasbos <mrlukasbos@gmail.com>
2020-09-07 16:15:06 +0200

    some more testing

--- src/test/java/SocketServerTest.java
+++ src/test/java/SocketServerTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,17 +14,19 @@ public class SocketServerTest {
     GameManager gameManager = new GameManager(socketServer, websocketServer);
 
     @Test
-    void itConnectsNewConnections() throws InterruptedException {
+    void itConnectsNewConnections() throws InterruptedException, IOException {
         socketServer.start(gameManager);
         assertEquals(socketServer.getConnections().size(), 0);
 
+        Socket s = null;
+
         try {
-            Socket s = new Socket("localhost", 10006);
+            s = new Socket("localhost", 10006);
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
             dout.writeUTF("Hello");
             dout.flush();
-            dout.close();
-            s.close();
+           // dout.close();
+           // s.close();
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -33,5 +38,15 @@ public class SocketServerTest {
             i++;
             if (i > 20) fail();
         }
+
+        while (socketServer.getConnectedPlayers().size() != 1) {
+            Thread.sleep(50);
+            i++;
+            if (i > 20) fail();
+        }
+       // assertEquals("Hello", socketServer.getConnectedPlayers().get(0).getName());
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        assertNotEquals("", in.readLine());
     }
 }
